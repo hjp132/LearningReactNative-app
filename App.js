e6a613616d39d036e9126411d6b26b848080e010
harry <harrypalmer889@gmail.com>
2020-08-22 15:43:16 +0100

    section 5.2

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
-import ComponentScreen from './src/screens/ComponentsScreen';
 import CompontentsScreen from './src/screens/ComponentsScreen';
 import ListScreen from './src/screens/ListScreen';
+import ImageScreen from './src/screens/ImageScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: CompontentsScreen,
-    List: ListScreen
+    List: ListScreen,
+    Image: ImageScreen
   },
   {
     initialRouteName: "Home",
