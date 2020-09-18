e674dfb7170bf344d01bdd9163cbd846edca4a28
harry <harrypalmer889@gmail.com>
2020-09-19 00:55:02 +0100

    section 6

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,21 +2,25 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity, ImagePropTypes } from "react-native";
 // import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = ({ navigation })=> {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
-      <Button 
-      onPress={() => navigation.navigate('Components')}
-      title="Go to Components Demo"/>
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title="Go to Components Demo" />
 
-      <Button 
-      onPress={() => navigation.navigate('List')}
-      title="Go to List Demo"/>
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title="Go to List Demo" />
 
-      <Button 
-      onPress={() => navigation.navigate('Image')}
-      title="Go to Image Screen"/>
+      <Button
+        onPress={() => navigation.navigate('Image')}
+        title="Go to Image Screen" />
+
+      <Button
+        onPress={() => navigation.navigate('Counter')}
+        title="Go to Counter" />
     </View>
 
   )
